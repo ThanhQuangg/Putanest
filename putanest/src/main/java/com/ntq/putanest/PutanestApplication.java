e5240010ad4d6fc82090913e602d46e7a5553b31
ThanhQuangg <thanhquang633@gmail.com>
2025-01-15 14:59:36 +0700

    update 15/01

--- putanest/src/main/java/com/ntq/putanest/PutanestApplication.java
+++ putanest/src/main/java/com/ntq/putanest/PutanestApplication.java
@@ -2,12 +2,22 @@ package com.ntq.putanest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
+
+//@SpringBootApplication
+//public class PutanestApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(PutanestApplication.class, args);
+//    }
+//
+//}
 
 @SpringBootApplication
 public class PutanestApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(PutanestApplication.class, args);
+        SpringApplication app = new SpringApplication(PutanestApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+        app.run(args);
     }
-
 }
