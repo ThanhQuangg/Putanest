64fd334931e3a0b9df1e01fcf4c6533af69936e6
ThanhQuangg <thanhquang633@gmail.com>
2025-02-18 20:44:14 +0700

    update vite.config.json

--- Website/putanest/vite.config.js
+++ Website/putanest/vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     "process.env": {}, // Polyfill process.env
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": {}, // Polyfill process.env
+  base: '/', // Đảm bảo URL đúng khi deploy
+  build: {
+    outDir: 'dist' // Vercel sẽ lấy thư mục này để deploy
   },
+  server: {
+    port: 3000
+  }
 });
+
