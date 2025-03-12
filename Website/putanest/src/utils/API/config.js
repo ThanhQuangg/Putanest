6b8e57f9f388b866ae4c839751ac68f4f8d296a3
ThanhQuangg <thanhquang633@gmail.com>
2025-03-12 10:52:13 +0700

    update

--- Website/putanest/src/utils/API/config.js
+++ Website/putanest/src/utils/API/config.js
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost:8080';
-// export const URL = 'https://putanest-h9ou.onrender.com'
+// export const URL = 'http://localhost:8080';
+export const URL = 'https://putanest-h9ou.onrender.com'
